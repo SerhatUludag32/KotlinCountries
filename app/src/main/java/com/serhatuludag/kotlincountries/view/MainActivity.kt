83aa5b5a9e3a9e599c74f03ec45e63c227e6487a
Serhat Uludag <serhat3264@gmail.com>
2024-11-28 12:50:18 +0300

    Layout Completed

--- app/src/main/java/com/serhatuludag/kotlincountries/view/MainActivity.kt
+++ app/src/main/java/com/serhatuludag/kotlincountries/view/MainActivity.kt
@@ -16,7 +16,6 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-            
         }
     }
 }
